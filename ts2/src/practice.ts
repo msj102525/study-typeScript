6fd795cfe5598d70c27b013ccc94a0af95671cee
DoorWinBell <msj102525@gmail.com>
2023-07-17 17:22:42 +0900

    1day

---
+++ ts2/src/practice.ts
@@ -0,0 +1,35 @@
+// const message: string = 'hello world';
+// console.log(message)
+
+// let count = 0; // 숫자
+// count += 1;
+// count = '갑자기 분위기 문자열'; // 이러면 에러가 납니다!
+
+// const message: string = 'hello world'; // 문자열
+
+// const done: boolean = true; // 불리언 값
+
+// const numbers: number[] = [1, 2, 3]; // 숫자 배열
+// const messages: string[] = ['hello', 'world']; // 문자열 배열
+
+// messages.push(1); // 숫자 넣으려고 하면.. 안된다!
+
+// let mightBeUndefined: string | undefined = undefined; // string 일수도 있고 undefined 일수도 있음
+// let nullableNumber: number | null = null; // number 일수도 있고 null 일수도 있음
+
+// let color: 'red' | 'orange' | 'yellow' = 'red'; // red, orange, yellow 중 하나임
+// color = 'yellow';
+// color = 'green'; // 에러 발생!
+
+const sum = (x: number, y: number): number => {
+  return x + y;
+};
+
+console.log(sum(1, 2));
+
+const sumArray = (numbers: number[]): number => {
+  return numbers.reduce((acc, current) => acc + current, 0);
+};
+
+const total = sumArray([1, 2, 3, 4, 5]);
+console.log(total);
